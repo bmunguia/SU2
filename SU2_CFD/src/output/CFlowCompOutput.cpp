87a969163d594075098b49015b144fb6d025a378
bmunguia <bmunguia@stanford.edu>
2025-06-17 13:52:00 -0700

    add volume to mesh adaptation solution outputs

--- SU2_CFD/src/output/CFlowCompOutput.cpp
+++ SU2_CFD/src/output/CFlowCompOutput.cpp
@@ -302,22 +302,8 @@ void CFlowCompOutput::SetVolumeOutputFields(CConfig *config){
 
   AddCommonFVMOutputs(config);
 
-  // Anisotropic metric
-  if(config->GetCompute_Metric()) {
-    if (nDim == 2){
-      AddVolumeOutput("METRIC_XX", "Metric_xx", "METRIC", "x-x-component of the metric");
-      AddVolumeOutput("METRIC_XY", "Metric_xy", "METRIC", "x-y-component of the metric");
-      AddVolumeOutput("METRIC_YY", "Metric_yy", "METRIC", "y-y-component of the metric");
-    }
-    else{
-      AddVolumeOutput("METRIC_XX", "Metric_xx", "METRIC", "x-x-component of the metric");
-      AddVolumeOutput("METRIC_XY", "Metric_xy", "METRIC", "x-y-component of the metric");
-      AddVolumeOutput("METRIC_YY", "Metric_yy", "METRIC", "y-y-component of the metric");
-      AddVolumeOutput("METRIC_XZ", "Metric_xz", "METRIC", "x-z-component of the metric");
-      AddVolumeOutput("METRIC_YZ", "Metric_yz", "METRIC", "y-z-component of the metric");
-      AddVolumeOutput("METRIC_ZZ", "Metric_zz", "METRIC", "z-z-component of the metric");
-    }
-  }
+  // Metric tensor and dual-cell volume
+  AddMeshAdaptationOutputs(config);
 
   if (config->GetTime_Domain()) {
     SetTimeAveragedFields();
@@ -399,21 +385,7 @@ void CFlowCompOutput::LoadVolumeData(CConfig *config, CGeometry *geometry, CSolv
 
   LoadCommonFVMOutputs(config, geometry, iPoint);
 
-  if(config->GetCompute_Metric()) {
-    if (nDim == 2){
-      SetVolumeOutputValue("METRIC_XX", iPoint, Node_Flow->GetMetric(iPoint, 0));
-      SetVolumeOutputValue("METRIC_XY", iPoint, Node_Flow->GetMetric(iPoint, 1));
-      SetVolumeOutputValue("METRIC_YY", iPoint, Node_Flow->GetMetric(iPoint, 2));
-    }
-    else{
-      SetVolumeOutputValue("METRIC_XX", iPoint, Node_Flow->GetMetric(iPoint, 0));
-      SetVolumeOutputValue("METRIC_XY", iPoint, Node_Flow->GetMetric(iPoint, 1));
-      SetVolumeOutputValue("METRIC_YY", iPoint, Node_Flow->GetMetric(iPoint, 2));
-      SetVolumeOutputValue("METRIC_XZ", iPoint, Node_Flow->GetMetric(iPoint, 3));
-      SetVolumeOutputValue("METRIC_YZ", iPoint, Node_Flow->GetMetric(iPoint, 4));
-      SetVolumeOutputValue("METRIC_ZZ", iPoint, Node_Flow->GetMetric(iPoint, 5));
-    }
-  }
+  LoadMeshAdaptationOutputs(config, solver, geometry, iPoint);
 
   if (config->GetTime_Domain()) {
     LoadTimeAveragedData(iPoint, Node_Flow);
--- SU2_CFD/src/output/CFlowOutput.cpp
+++ SU2_CFD/src/output/CFlowOutput.cpp
@@ -4103,3 +4103,47 @@ void CFlowOutput::AddTurboOutput(unsigned short nZone){
   AddHistoryOutput("KineticEnergyLoss_Stage", "KELC_all", ScreenOutputFormat::SCIENTIFIC, "TURBO_PERF", "Machine Kinetic Energy Loss Coefficient", HistoryFieldType::DEFAULT);
   AddHistoryOutput("TotPressureLoss_Stage", "TPLC_all", ScreenOutputFormat::SCIENTIFIC, "TURBO_PERF", "Machine Pressure Loss Coefficient", HistoryFieldType::DEFAULT);
 }
+
+void CFlowOutput::AddMeshAdaptationOutputs(const CConfig* config) {
+
+  // Anisotropic metric tensor, and dual-cell volume
+  if(config->GetCompute_Metric()) {
+    if (nDim == 2){
+      AddVolumeOutput("METRIC_XX", "Metric_xx", "MESH_ADAPT", "x-x-component of the metric");
+      AddVolumeOutput("METRIC_XY", "Metric_xy", "MESH_ADAPT", "x-y-component of the metric");
+      AddVolumeOutput("METRIC_YY", "Metric_yy", "MESH_ADAPT", "y-y-component of the metric");
+    }
+    else{
+      AddVolumeOutput("METRIC_XX", "Metric_xx", "MESH_ADAPT", "x-x-component of the metric");
+      AddVolumeOutput("METRIC_XY", "Metric_xy", "MESH_ADAPT", "x-y-component of the metric");
+      AddVolumeOutput("METRIC_YY", "Metric_yy", "MESH_ADAPT", "y-y-component of the metric");
+      AddVolumeOutput("METRIC_XZ", "Metric_xz", "MESH_ADAPT", "x-z-component of the metric");
+      AddVolumeOutput("METRIC_YZ", "Metric_yz", "MESH_ADAPT", "y-z-component of the metric");
+      AddVolumeOutput("METRIC_ZZ", "Metric_zz", "MESH_ADAPT", "z-z-component of the metric");
+    }
+    AddVolumeOutput("VOLUME", "Volume", "MESH_ADAPT", "Dual-cell volume");
+  }
+}
+
+void CFlowOutput::LoadMeshAdaptationOutputs(const CConfig* config, const CSolver* const* solver, const CGeometry* geometry, 
+                                            unsigned long iPoint) {
+
+  const auto* Node_Flow = solver[FLOW_SOL]->GetNodes();
+  const auto* Node_Geo = geometry->nodes;
+  if(config->GetCompute_Metric()) {
+    if (nDim == 2){
+      SetVolumeOutputValue("METRIC_XX", iPoint, Node_Flow->GetMetric(iPoint, 0));
+      SetVolumeOutputValue("METRIC_XY", iPoint, Node_Flow->GetMetric(iPoint, 1));
+      SetVolumeOutputValue("METRIC_YY", iPoint, Node_Flow->GetMetric(iPoint, 2));
+    }
+    else{
+      SetVolumeOutputValue("METRIC_XX", iPoint, Node_Flow->GetMetric(iPoint, 0));
+      SetVolumeOutputValue("METRIC_XY", iPoint, Node_Flow->GetMetric(iPoint, 1));
+      SetVolumeOutputValue("METRIC_YY", iPoint, Node_Flow->GetMetric(iPoint, 2));
+      SetVolumeOutputValue("METRIC_XZ", iPoint, Node_Flow->GetMetric(iPoint, 3));
+      SetVolumeOutputValue("METRIC_YZ", iPoint, Node_Flow->GetMetric(iPoint, 4));
+      SetVolumeOutputValue("METRIC_ZZ", iPoint, Node_Flow->GetMetric(iPoint, 5));
+    }
+    SetVolumeOutputValue("VOLUME", iPoint, Node_Geo->GetVolume(iPoint));
+  }
+}
