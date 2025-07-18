b98d697cf0fe6cae347e940c7a565177e04bc40c
bmunguia <bmunguia@stanford.edu>
2025-07-18 14:32:57 -0700

    Add GMF mesh/solution filewriters

---
+++ SU2_CFD/include/output/filewriter/CGMFMeshFileWriter.hpp
@@ -0,0 +1,68 @@
+/*!
+ * \file CGMFMeshFileWriter.hpp
+ * \brief Headers for GMF mesh file writer class.
+ * \author B. Munguía
+ * \version 8.2.0 "Harrier"
+ *
+ * SU2 Project Website: https://su2code.github.io
+ *
+ * The SU2 Project is maintained by the SU2 Foundation
+ * (http://su2foundation.org)
+ *
+ * Copyright 2012-2025, SU2 Contributors (cf. AUTHORS.md)
+ *
+ * SU2 is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * SU2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SU2. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#ifdef HAVE_CGNS
+extern "C" {
+#include "libmeshb7.h"
+}
+#endif
+
+#include "CFileWriter.hpp"
+
+class CGMFMeshFileWriter final: public CFileWriter{
+
+private:
+  unsigned short iZone, //!< Index of the current zone
+  nZone;                //!< Number of zones
+#ifdef HAVE_GMF
+#endif
+
+public:
+
+  /*!
+   * \brief File extension
+   */
+  const static string fileExt;
+
+  /*!
+   * \brief Construct a file writer using field names, dimension.
+   * \param[in] valDataSorter - The parallel sorted data to write
+   * \param[in] valiZone - The index of the current zone
+   * \param[in] valnZone - The total number of zones
+   */
+  CGMFMeshFileWriter(CParallelDataSorter* valDataSorter,
+                     unsigned short valiZone, unsigned short valnZone);
+
+  /*!
+   * \brief Write sorted data to file in GMF mesh file format
+   * \param[in] val_filename - The name of the file
+   */
+  void WriteData(string val_filename) override ;
+
+};
---
+++ SU2_CFD/src/output/filewriter/CGMFFileWriter.cpp
@@ -0,0 +1,43 @@
+/*!
+ * \file CGMFFileWriter.hpp
+ * \brief Filewriter class for GMF format solution.
+ * \author B. Munguía
+ * \version 8.2.0 "Harrier"
+ *
+ * SU2 Project Website: https://su2code.github.io
+ *
+ * The SU2 Project is maintained by the SU2 Foundation
+ * (http://su2foundation.org)
+ *
+ * Copyright 2012-2025, SU2 Contributors (cf. AUTHORS.md)
+ *
+ * SU2 is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * SU2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SU2. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "../../../include/output/filewriter/CGMFFileWriter.hpp"
+
+const string CGMFFileWriter::fileExt = ".solb";
+
+CGMFFileWriter::CGMFFileWriter(CParallelDataSorter *valDataSorter)  :
+  CFileWriter(valDataSorter, fileExt){}
+
+
+CGMFFileWriter::~CGMFFileWriter()= default;
+
+void CGMFFileWriter::WriteData(string val_filename){
+
+#ifdef HAVE_GMF
+
+#endif
+}
