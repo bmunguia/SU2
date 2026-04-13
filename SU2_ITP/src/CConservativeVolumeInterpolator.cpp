c4e73dd9ec34a293b6f9a063f7e307ecdbaf5c07
bmunguia <bmunguia@stanford.edu>
2026-04-13 13:48:45 -0700

    Rename "Derived" to "Computed" sensors

--- SU2_ITP/src/CConservativeVolumeInterpolator.cpp
+++ SU2_ITP/src/CConservativeVolumeInterpolator.cpp
@@ -99,7 +99,7 @@ void CConservativeVolumeInterpolator::Postprocess(CConfig* config, CGeometry* ge
     const auto solver_index = config->GetContainerPosition(RUNTIME_FLOW_SYS);
     auto* solver_flow = solver_container_dst[solver_index];
     solver_flow->SetPrimitive_SensorAdapt(geometry_dst, config);
-    solver_flow->SetDerived_SensorAdapt(geometry_dst, config);
+    solver_flow->SetComputed_SensorAdapt(geometry_dst, config);
 
     /*--- Sync all sensor values (including custom Python sensors) to halo nodes
      *    before gradient/Hessian computation. Matches CSinglezoneDriver behavior. ---*/
